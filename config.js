db2be7cda42c7d5e3151fcfd99fbe8de15c82b97
Stéphane Gully <stephane.gully@gmail.com>
2014-04-15 13:54:23 +0200

    first README + rename imap to gmail in the config to be more explicit

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 
 module.exports = {
-  "imap": {
-    "user": "xxx@gmail.com",
-    "password": "xxx",
-    "host": "imap.gmail.com",
-    "port": 993,
-    "tls": true
+  "gmail": {
+    "user": "xxx@gmail.com",  // your gmail adress
+    "password": "xxx",        // your gmail password
+    "host": "imap.gmail.com", // gmail imap hostname
+    "port": 993,              // gmail imap port
+    "tls": true               // imap using tls
   },
-  "pingDelay": 1000*60*5, // 5 minutes before runing a new ping
-  "pongDelay": 1000*10,   // 10 secondes before checking the IMAP box after the ping is sent 
+  "pingDelay":   1000*60*5, // 5 minutes before runing a new ping
+  "pongDelay":   1000*10,   // 10 secondes before checking the IMAP box after the ping is sent 
+  "nbPongRetry": 3,         // number of pong retry before considering the mail is not received
 }
