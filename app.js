bb18e733cd5bd8829ab2e95d306bd414c71fe111
Stéphane Gully <stephane.gully@gmail.com>
2014-04-18 22:45:04 +0200

    better console.log in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
+var config   = require('./lib/config.js');
 var emailAck = require('./lib/emailack.js');
-var express = require('express');
+var express  = require('express');
 
 var app = express();
 app.enable('trust proxy');
@@ -11,11 +11,12 @@ app.get('/', function (req, res){
 
 // web server errors handling
 app.use(function (err, req, res, next){
-  console.error (err.stack);
+  console.error(err.stack);
   res.send(500, 'Something broke!');
 });
 var server = app.listen(3000, function () {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Email will be sent to %s[%s] each %dms', config.gmail.user, config.gmail.mailbox, config.pingDelay);
+  console.log('REST API listening on http://127.0.0.1:%d', server.address().port);
 });
 
 // start email ping pong
