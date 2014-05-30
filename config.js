4dfe3435cdd52468df96ca90fe2dee83f1d7e444
Stéphane Gully <stephane.gully@gmail.com>
2014-05-30 22:02:27 +0200

    change default to 30 secondes before checking new mail

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
     "tls": true               // imap using tls
   },
   "pingDelay":   1000*60*5, // 5 minutes before runing a new ping
-  "pongDelay":   1000*10,   // 10 secondes before checking the IMAP box after the ping is sent 
+  "pongDelay":   1000*30,   // 30 secondes before checking the IMAP box after the ping is sent 
   "nbPongRetry": 3,         // number of pong retry before considering the mail is not received
 }
